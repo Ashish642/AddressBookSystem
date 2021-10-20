f7b368b26180bc9d7ad200a44257033979477bfe
Ashish <ashishkumarvish.432@gmail.com>
2021-10-20 15:16:30 +0530

    Creat contact

--- src/com/bridgelab/AddressBookSystem.java
+++ src/com/bridgelab/AddressBookSystem.java
@@ -3,6 +3,14 @@ package com.bridgelab;
 public class AddressBookSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book ");
-        
+        Person person = new Person("Ashish" , "kumar", "Allahabad,UP", "Up", 7652042754L, "211101",);
+        System.out.println("Firstname: " + person.getFirstName());
+        System.out.println("LastName:" + person.getLastName());
+        System.out.println("Address: "+ person.getAddress());
+        System.out.println("City:" + person.getCity());
+        System.out.println("State :" + person.getState());
+        System.out.println("Phone number :" + person.getPhoneNumber());
+        System.out.println("Zip :" + person.getZip());
+
     }
 }
